f89b2cac05af7ed0b6a64fa21b28808609b41794
omfhxll <dmitriy.drogin@mail.ru>
2024-08-23 17:07:17 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
 import './App.css';
-const tg = window.Telegram.WebApp;
 
 function App() {
   return (
     <div className="App">
       <p>id: {tg.initDataUnsafe.user.id} </p>
-      <p>username: {tg.initDataUnsafe.user.username} </p>
-      <p>isBot: {tg.initDataUnsafe.user.is_bot ? "Бот" : "Не бот"} </p>
-      <p>isBot: {tg.initDataUnsafe.user.is_premium ? "Премиум" : "Стандартный"} </p>
-      <image src={tg.initDataUnsafe.user.photo_url} />
     </div>
   );
 }
