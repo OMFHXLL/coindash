6594c23e7501145a404cb8213166d5f2689f9a1e
omfhxll <dmitriy.drogin@mail.ru>
2024-08-23 16:37:07 +0300

    init

--- src/App.js
+++ src/App.js
@@ -4,7 +4,11 @@ const tg = window.Telegram.WebApp;
 function App() {
   return (
     <div className="App">
-      <p> {tg.initDataUnsafe.user.username} </p>
+      <p>id: {tg.initDataUnsafe.user.id} </p>
+      <p>username: {tg.initDataUnsafe.user.username} </p>
+      <p>isBot: {tg.initDataUnsafe.user.is_bot ? "Бот" : "Не бот"} </p>
+      <p>isBot: {tg.initDataUnsafe.user.is_premium ? "Премиум" : "Стандартный"} </p>
+      <image src={tg.initDataUnsafe.user.photo_url} />
     </div>
   );
 }
