e0d7d1db91c0e0dbdee2e9eb1c7f3acec6743e86
omfhxll <dmitriy.drogin@mail.ru>
2024-08-27 05:02:26 +0300

    beta1.2

--- src/hooks/useTelegram.js
+++ src/hooks/useTelegram.js
@@ -16,13 +16,13 @@ if (!isTg) {
   }
 } else {
   tgUser = {
-    id: tg.initDataUnsafe.user.id,
-    username: tg.initDataUnsafe.user.username,
-    first_name: tg.initDataUnsafe.user.fisrtName,
-    last_name: tg.initDataUnsafe.user.lastName,
-    is_bot: tg.initDataUnsafe.user.is_bot,
-    is_premium: tg.initDataUnsafe.user.is_premium,
-    language: tg.initDataUnsafe.user.language_code
+    id: tg?.initDataUnsafe?.user?.id,
+    username: tg?.initDataUnsafe?.user?.username,
+    first_name: tg?.initDataUnsafe?.user?.fisrtName,
+    last_name: tg?.initDataUnsafe?.user?.lastName,
+    is_bot: tg?.initDataUnsafe?.user?.is_bot,
+    is_premium: tg?.initDataUnsafe?.user?.is_premium,
+    language: tg?.initDataUnsafe?.user?.language_code
   }
 }
 
